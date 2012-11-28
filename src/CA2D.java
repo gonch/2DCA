ef9519d3534e6087c191ecdfb4fa854c298faec5
gonch <gonchos@gmail.com>
2012-11-28 15:44:52 +0100

    GA is working!
    fitness = attractor length

--- src/CA2D.java
+++ src/CA2D.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
@@ -8,6 +9,7 @@ public class CA2D {
 	public ArrayList<List<Integer>> organism;
 	public int [][][][][] transitionMatrix;
 	int organismSize;
+	double fitness;
 	
 	public CA2D(int organismSize)
 	{
@@ -19,6 +21,11 @@ public class CA2D {
 		organism.get(organismSize/2).set(organismSize/2,1);
 		this.organismSize = organismSize;
 		transitionMatrix = new int [3][3][3][3][3];
+	}
+	
+	public double getFitness()
+	{
+		return fitness;
 	}
 	
 	public void randomizeTransitionMatrix()
@@ -51,6 +58,54 @@ public class CA2D {
 			}
 			System.out.println("");
 		}
+	}
+	
+	public CA2D mutateOrganism()
+	{
+		CA2D mutatedCA2D = new CA2D(4);
+		int random = 0;
+		int oldvalue = 0;
+		for(int i = 0;i < 3; i++)
+		{
+			for(int j = 0;j < 3; j++)
+			{
+				for(int k = 0;k < 3; k++)
+				{
+					for(int l = 0;l < 3; l++)
+					{
+						for(int m = 0;m < 3; m++)
+						{
+							random = (int)(Math.random() * 50);
+							if(random == 0)
+							{
+								
+								oldvalue = transitionMatrix[i][j][k][l][m];
+								if(oldvalue == 0)
+								{
+									double randomDouble = (Math.random());
+									if(randomDouble < 0.5) mutatedCA2D.transitionMatrix[i][j][k][l][m] = 1;
+									else mutatedCA2D.transitionMatrix[i][j][k][l][m] = 2;
+								}
+								else if (oldvalue == 1)
+								{
+									double randomDouble = (Math.random());
+									if(randomDouble < 0.5) mutatedCA2D.transitionMatrix[i][j][k][l][m] = 0;
+									else mutatedCA2D.transitionMatrix[i][j][k][l][m] = 2;
+								}
+								else
+								{
+									double randomDouble = (Math.random());
+									if(randomDouble < 0.5) mutatedCA2D.transitionMatrix[i][j][k][l][m] = 0;
+									else mutatedCA2D.transitionMatrix[i][j][k][l][m] = 1;
+								}
+							}
+							else mutatedCA2D.transitionMatrix[i][j][k][l][m] = transitionMatrix[i][j][k][l][m];
+						}
+					}
+				}
+			}
+		}
+		return mutatedCA2D;
 	}
 	
 	public void writeTransition (int top, int bottom, int left, int right, int centre, int value)
@@ -101,7 +156,7 @@ public class CA2D {
 		
 	}
 	
-	public double fitness1()
+	public double fitness1()//compare to total attractor length of the population also majority
 	{
 		double Majority = this.calculateMajority();
 		int result[] = this.calculateAttractor();
@@ -109,6 +164,12 @@ public class CA2D {
 		if (attractor > 100000) attractor = 100000;
 		attractor = attractor/100000;	
 		return (Majority+attractor)/2;	
+	}
+	
+	public int fitness2()
+	{
+		int result[] = this.calculateAttractor();
+		return result[1];
 	}
 	
 	public int[] calculateAttractor()
@@ -195,44 +256,39 @@ public class CA2D {
 	{
 		long startTime = System.nanoTime();
 		int size = 4;
-		int numberOfGenerations = 100;
+		int numberOfGenerations = 10000;
 		CA2D generation[] = new CA2D[10];
-		double totalFitness = 0;
-		double fitness[] = new double[10];
-		HashMap<Double,Integer> map = new HashMap<Double,Integer>();
 		for(int i = 0; i <10; i++) //creates random population of intial organism and fills the first map
 		{
-			System.out.println("****************************************");
-			System.out.println("CA #" +i);
+//			System.out.println("****************************************");
+//			System.out.println("CA #" +i);
 			generation[i] = new CA2D(size);
-			generation[i].randomizeTransitionMatrix();
-			fitness[i] = -1*generation[i].fitness1();
-			System.out.println("FITNESS = "+ -1*fitness[i]);
-			System.out.println("****************************************");
-			totalFitness += -1*fitness[i];
-			map.put(-1*fitness[i], i);
+			//generation[i].randomizeTransitionMatrix();
+			int result[] = generation[i].calculateAttractor();
+			int attractor = result[1];
+			generation[i].fitness = attractor;
+			//fitness[i] = -1*attractor;
+			//fitness[i] = -1*generation[i].fitness1();
+//			System.out.println("FITNESS = "+ -1*fitness[i]);
+//			System.out.println("FITNESS = "+ generation[i].fitness);
+//			System.out.println("****************************************");
+//			totalFitness += -1*fitness[i];
+//			map.put(-1*fitness[i], i);
 		}
 		CA2D generation2[] = new CA2D[10];
-		long time1,time2,time3;
-		time1=0;
-		time2=0;
-		time3=0;
 		for(int i = 0;i < numberOfGenerations;i++)
 		{
 //			System.out.println("****************************************");
-			if(i%100==0)System.out.println("GENERATION "+i);
-			generation2 = evoStep(generation,map,fitness,totalFitness,time1,time2,time3);
+//			if(generation[i])
+			if(i%100==0) System.out.println("GENERATION "+i);
+			generation2 = evoStep(generation);
 //			System.out.println("****************************************");
 		}
-		double sortedFitness[]  = new double[10];;
+		CA2DComparator comparator = new CA2DComparator();
+		Arrays.sort(generation, comparator);
 		for (int i = 0;i< generation2.length;i++)
 		{
-			sortedFitness[i] = -1*generation2[i].fitness1();
-		}
-		Arrays.sort(sortedFitness);
-		for (int i = 0;i< generation2.length;i++)
-		{
-			System.out.println("FINAL "+ i + " = " + sortedFitness[i]*-1);
+			System.out.println("FINAL "+ i + " = " + generation[i].fitness);
 		}
 		long endTime = System.nanoTime();
 		long duration = endTime - startTime;
@@ -284,36 +340,32 @@ public class CA2D {
 		
 	}
 
-	private static CA2D[] evoStep(CA2D[] generation,
-			HashMap<Double, Integer> map, double[] fitness, double totalFitness,long time1, long time2, long time3) {
-		CA2D TNG[]= new CA2D[10];
-		Arrays.sort(fitness);
-		for(int i = 0;i<fitness.length;i++) 
-		{
-			fitness[i]*=-1;
-		}
+	private static CA2D[] evoStep(CA2D[] generation) {		
+		CA2DComparator comparator = new CA2DComparator();
+		
+		Arrays.sort(generation, comparator);
 		double normalizedFitness[] = new double[10];
-		double leap = 0.2;
-		for (int i = 0;i < fitness.length;i++)
+		double leap = 0.5; // (1/number of selected organisms)
+		double totalFitness = 0;
+		for (int i = 0;i < generation.length;i++)
 		{
-			if(i!=0) 
-			{
-				normalizedFitness[i]= normalizedFitness[i-1] + (fitness[i]/totalFitness);
-			}
-			else normalizedFitness[i] = fitness[i]/totalFitness;
+			totalFitness += generation[i].fitness;
+		}
+		for (int i = 0;i < generation.length;i++)
+		{
+			if(i!=0) normalizedFitness[i]= normalizedFitness[i-1] + (generation[i].fitness/totalFitness);
+			else normalizedFitness[i] = generation[i].fitness/totalFitness;
 //			System.out.println("NORMALIZED FITNESS " + i + " = " + normalizedFitness[i]);
 		}
 		Random r = new Random();
 		double randomStart = 0 + (leap - 0) * r.nextDouble();
 		int chosen = 0;
-//		System.out.println("HASHMAP AT START" + map.toString());
-//		System.out.println("HASHMAP SIZE AT START" + map.size());
+		int chosen1 = -1;
+		int chosen2 = -1;
 //		System.out.println("randomStart " + randomStart);
 //		System.out.println("leap = " + leap);
-		long start = System.nanoTime();
-		for(int i = 0; i <5; i++) //select the five strongest with Stochastic universal sampling
+		for(int i = 0; i <2; i++) //select the TWO strongest with Stochastic universal sampling
 		{
-//			double auxiliarFitness;
 //			System.out.println("ITERATION " + i);
 			double pointer = randomStart+(i*leap);
 //			System.out.println("POINTER = " + pointer);
@@ -321,57 +373,69 @@ public class CA2D {
 			{
 //				System.out.println("<------ Ignore organism  " + chosen);
 //				System.out.println("KEY TO DELETE " + fitness[chosen]);
-				map.remove(fitness[chosen]);
+//				map.remove(fitness[chosen]);
 //				System.out.println("ELEMENT DELETED, NEW SIZE = " + map.size());
 				chosen++;
 				if(chosen>=10)break;
 			}
 			if(chosen>=10)break;
 //			System.out.println("----->Take organism " + chosen);
-//			System.out.println("ORIGINAL organism = " +  map.get(fitness[chosen]));
-			TNG[i] = generation[map.get(fitness[chosen])];//Mirar esta linea... chosen o i? ojo!
-//			map.remove(fitness[chosen]);
-			map.put(fitness[chosen], i);
-//			auxiliarFitness = Math.abs(fitness[chosen]);
-//			System.out.println("NEW HASHMAP" + map.toString());
-//			System.out.println("ELEMENT REASSIGNED - SIZE OF THE MAP = " + map.size());
+			if(chosen1==-1) chosen1 = chosen;
+			else chosen2 = chosen;
+//			TNG[i] = generation[map.get(fitness[chosen])];//Mirar esta linea... chosen o i? ojo!;
 			chosen++;
 		}
-		while(chosen < 10) {
-			map.remove(fitness[chosen]);
-			chosen++;
-		}
+		if(chosen1 == -1 || chosen2 ==-1) throw new IllegalArgumentException("chosen 1 or chosen 2 not valid");
+//		System.out.println("CHOSEN 1 = "+chosen1+" - CHOSEN 2 = "+chosen2);
 //		System.out.println("MAP SIZE = "+map.size());
-		long finish = System.nanoTime();
-		time1 += (finish-start);
-		start = System.nanoTime();
-		for(int i = 5; i < TNG.length;i++) //fill the other five with combinations of the first five
+		int worst = 9;
+		while (worst == chosen2 || worst == chosen1)
 		{
-			int random1 = (int)(Math.random() * 5);
-			int random2 = (int)(Math.random() * 5);
-			while(random1==random2) random2 = (int)(Math.random() * 5);		
-			TNG[i] = offspring(TNG[random1],TNG[random2]);
+			worst--;
+			if(worst==0) throw new IllegalArgumentException("worst out of range");
 		}
-		finish = System.nanoTime();
-		time2 += (finish-start);
-		start = System.nanoTime();
-		map.clear();
-		for(int i = 0; i <10; i++) //update fitness values and map
+//		System.out.println("WORST =" + worst);
+		for(int i = 0; i < 2; i++) { // delete the two worst and replace for two mutations of chosen1
+			generation[worst] = generation[chosen1].mutateOrganism();
+			generation[worst].fitness = generation[worst].fitness2(); //attractor as fitness
+//			System.out.println(worst+" organism deleted by mutation of chosen 1, new organism fitness = "+generation[worst].fitness);
+			worst--;
+			while (worst == chosen2 || worst == chosen1)
+			{
+				worst--;
+				if(worst==0) throw new IllegalArgumentException("worst out of range");
+			}
+		}
+		for(int i = 0; i < 2; i++) { // delete the two worst and replace for two mutations of chosen2
+			generation[worst] = generation[chosen2].mutateOrganism();
+			generation[worst].fitness = generation[worst].fitness2(); //attractor as fitness
+//			System.out.println(worst+" organism deleted by mutation of chosen 2, new organism fitness = "+generation[worst].fitness);
+			worst--;
+			while (worst == chosen2 || worst == chosen1)
+			{
+				worst--;
+				if(worst==0) throw new IllegalArgumentException("worst out of range");
+			}
+		}
+		generation[worst]=offspring(generation[chosen1],generation[chosen2]);
+		generation[worst].fitness = generation[worst].fitness2(); //attractor as fitness
+
+//		System.out.println(worst+" organism deleted by crossover, new organism fitness = "+generation[worst].fitness);
+//		System.out.println(worst+" organism deleted by offspring");
+//		System.out.println("NEXT GENERATION FITNESS");
+//		for(int i = 0;i< generation.length;i++)
 		{
-			fitness[i] = -1*generation[i].fitness1();
-			totalFitness += -1*fitness[i];
-			map.put(-1*fitness[i], i);
+//			System.out.println(generation[i].fitness);
 		}
-		finish = System.nanoTime();
-		time3 += (finish-start);
-//		System.out.println("TIME 1 = "+time1);
-//		System.out.println("TIME 2 = "+time2);
-//		System.out.println("TIME 3 = "+time3);
-		return TNG;
+		return generation;
 	}
 
-	private static CA2D offspring(CA2D father, CA2D mother) {
+	private static CA2D offspring(CA2D father, CA2D mother) {//split into two parts
 		CA2D child = new CA2D(4);
+		double randomDouble = (Math.random());
+		boolean fatherFirst  = false;
+		if(randomDouble < 0.5) fatherFirst = true;
+		int counter = 0;
 		for(int i = 0;i < 3; i++)
 		{
 			for(int j = 0;j < 3; j++)
@@ -382,9 +446,17 @@ public class CA2D {
 					{
 						for(int m = 0;m < 3; m++)
 						{
-							double random = (Math.random());
-							if(random < 0.5) child.transitionMatrix[i][j][k][l][m] = father.transitionMatrix[i][j][k][l][m];
-							else child.transitionMatrix[i][j][k][l][m] = mother.transitionMatrix[i][j][k][l][m];
+							counter++;
+							if(fatherFirst)
+							{
+								if(counter<121) child.transitionMatrix[i][j][k][l][m] = father.transitionMatrix[i][j][k][l][m];
+								else child.transitionMatrix[i][j][k][l][m] = mother.transitionMatrix[i][j][k][l][m];
+							}
+							else
+							{
+								if(counter<121)	child.transitionMatrix[i][j][k][l][m] = mother.transitionMatrix[i][j][k][l][m];
+								else child.transitionMatrix[i][j][k][l][m] = father.transitionMatrix[i][j][k][l][m];
+							}
 						}
 					}
 				}
@@ -392,4 +464,30 @@ public class CA2D {
 		}
 		return child;
 	}
+	
+	public static class CA2DComparator implements Comparator<CA2D>{
+		 
+	    @Override
+	    public int compare(CA2D CA2D1, CA2D CA2D2) {
+	 
+	        double fitness1 = CA2D1.fitness;
+	        double fitness2 = CA2D2.fitness;
+	 
+	        if (fitness1 < fitness2)return +1;
+	        else if (fitness1 > fitness2)return -1;
+	        else return 0;
+	    }
+	}
+	// mutation=1/N chance [DONE] - changed to 2%
+	// offspring done by taking half from father and half from mother [DONE]
+	// select 2 organisms, mutate them twice and replace the 4 worse, crossover and generate another one replacing the worst left;
+	// for each generation, best distance vs generation , best parameter vs generation (average and deviation)
+	// allways keep the best organism
+	// --- 10 tests with GA to plot generations vs fitness (average and deviation). each generation, save best, worst and average 
+	// add target and see how many generations it takes to get there (500,1000,15000,20000,100000(impossible)) 50k generation cap
+	// discard with majority -> thershold of difference: 
+	// calcaulate majority comparing best to mutated mutated and crossover (+-0.02) if it does not get in the interval, mutate again
+	// mutate crossover too!;
+	// fitness 2 = normalize attractor with sum of all, and same for majority then 50% each
+	// in target graph, fitness is target-best
 }
